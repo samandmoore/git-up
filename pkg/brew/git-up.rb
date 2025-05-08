d7f2f5771954c80b1928e86589ae828bb108894d
Sam Moore <sam@betterment.com>
2025-05-08 12:45:24 -0400

    Update git-up.rb

--- pkg/brew/git-up.rb
+++ pkg/brew/git-up.rb
@@ -1,10 +1,10 @@
 class GitUp < Formula
-  version 'v0.1.0'
+  version 'v0.2.0'
   desc 'Git up command to fetch and update all local branches that track remotes.'
   homepage 'https://github.com/samandmoore/git-up'
 
   if OS.mac?
-    url "https://github.com/samandmoore/git-up/releases/download/#{version}/git-up-#{version}-x86_64-apple-darwin.tar.gz"
+    url "https://github.com/samandmoore/git-up/releases/download/#{version}/git-up-#{version}-aarch64-apple-darwin.tar.gz"
     sha256 'e4d230d180857dfa3ccea26176f666cf3e9d2652ee841d5a9b7c20e3768b6d39'
   end
 
